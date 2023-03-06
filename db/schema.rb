1f40653b3312e91fac4de63ed4c3cf7cc576fc88
Humberto <hdkaratechop@gmail.com>
2023-03-05 21:33:35 -0800

    run flight migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_043205) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_044105) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -18,4 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_043205) do
     t.index ["code"], name: "index_airports_on_code", unique: true
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.datetime "departure_time", null: false
+    t.integer "duration_in_min", null: false
+    t.integer "departure_airport_id", null: false
+    t.integer "arrival_airport_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
