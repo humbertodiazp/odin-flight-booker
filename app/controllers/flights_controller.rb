ae45f75d2734f673658b337c7136d1afee13d1da
Humberto Diaz <97567622+humbertodiazp@users.noreply.github.com>
2023-03-07 18:22:25 +0000

    add index controller for booking flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,24 @@
-class FlightsController < ApplicationController
-    
-    def index 
-        @flights = Flight.all
-    end
 
-end
+class FlightsController < ApplicationController
+    def index
+      @airport_options = Airport.all.map { |u| [u.location, u.id] }
+      return if search_params.empty?
+  
+      @booking_options = find_booking_options
+    end
+  
+    private
+  
+      def search_params
+        params.permit(:departure_airport_id, :arrival_airport_id, :departure_time)
+      end
+  
+      def find_booking_options
+        if params[:departure_airport_id] == params[:arrival_airport_id]
+          flash.now[:alert] = "Invalid selection, please try again!"
+          render :index
+        else
+          BookingOptions.new(search_params).find_flights
+        end
+      end
+  end
