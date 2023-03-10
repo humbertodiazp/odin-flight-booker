ae6ef446e6d377d764cc7d40b102a7833ad4c2ec
Humberto <hdkaratechop@gmail.com>
2023-03-09 21:37:32 -0800

    add new and create methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,22 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Flight.booking_options[search_params]  )
   end
-end
+
+  def create
+    @booking = Flight.booking_options[search_params]  )
+    
+    respond_to do |format|
+      if @booking.save
+        
+        format.html { redirect_to booking_url(@booking), notice: "Booking succesfully created." }
+        format.json { render :show, status: :created, location: @booking }
+        
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @booking.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  
