5db04d739efdc6c2353249d6289697520478082b
Humberto <hdkaratechop@gmail.com>
2023-03-15 23:04:27 -0700

    adjust search params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,24 +1,27 @@
-
 class FlightsController < ApplicationController
-    def index
-      @airport_options = Airport.all.map { |u| [u.location, u.id] }
-      return if search_params.empty?
-  
-      @booking_options = find_booking_options
+  def index
+    @airport_options = Airport.select(:airport, :id).order(:airport).page(params[:page])
+
+    if search_params.present?
+      if departure_airport == arrival_airport
+        flash.now[:alert] = "Departure and arrival airports cannot be the same"
+      else
+        @booking_options = BookingOptions.new(search_params).find_flights
+      end
     end
-  
-    private
-  
-      def search_params
-        params.permit(:departure_airport_id, :arrival_airport_id, :departure_time)
-      end
-  
-      def find_booking_options
-        if params[:departure_airport_id] == params[:arrival_airport_id]
-          flash.now[:alert] = "Invalid selection, please try again!"
-          render :index
-        else
-          BookingOptions.new(search_params).find_flights
-        end
-      end
   end
+
+  private
+
+  def search_params
+    params.permit(:departure_airport_id, :arrival_airport_id, :departure_time)
+  end
+
+  def departure_airport
+    @departure_airport ||= Airport.find_by(id: params[:departure_airport_id])
+  end
+
+  def arrival_airport
+    @arrival_airport ||= Airport.find_by(id: params[:arrival_airport_id])
+  end
+end
