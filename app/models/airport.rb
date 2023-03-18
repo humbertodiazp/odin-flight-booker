b001446ffb894ead47082b163e9728896f3c1b94
Humberto <hdkaratechop@gmail.com>
2023-03-18 16:38:07 -0700

    add airport code scope

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,8 @@
 class Airport < ApplicationRecord
-    has_many :flights
-    has_many :bookings
-end
+    default_scope { order(:code) }
+  
+    validates :code, presence: true, length: { is: 3 }
+  
+    has_many :departure_flights, class_name: 'Flight', foreign_key: 'departure_airport_id', inverse_of: :departure_airport
+    has_many :arrival_flights, class_name: 'Flight', foreign_key: 'arrival_airport_id', inverse_of: :arrival_airport
+  end
