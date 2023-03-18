840d3566e2708ac905d82e9ff8c52276b21efe0f
Humberto <hdkaratechop@gmail.com>
2023-03-18 16:30:53 -0700

    add flights and booking routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'flights#index'
+  resources :flights, only: %i[index]
+  resources :bookings, only: %i[new create show]
 end
