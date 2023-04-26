c849214f0ab392824bbc65923f62bc149754a06d
Humberto <hdkaratechop@gmail.com>
2023-04-25 23:25:35 -0700

    add more validations for flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,27 +1,49 @@
 class FlightsController < ApplicationController
-  def index
-    @airport_options = Airport.select(:airport, :name).order(:airport).page(params[:page])
+  before_action :set_flights, only: [:index]
 
-    if search_params.present?
-      if departure_airport == arrival_airport
-        flash.now[:alert] = "Departure and arrival airports cannot be the same"
+  def index
+    if params[:searching]
+      if all_search_params_present_and_not_empty? && given_valid_date? && given_upcoming_date? && !same_airports?
+        @flights = Flight.search(params)
       else
-        @booking_options = BookingOptions.new(search_params).find_flights
+        handle_search_error
       end
     end
   end
 
   private
 
-  def search_params
-    params.permit(:departure_airport, :arrival_airport, :departure_time)
+  def set_flights
+    @flights = Flight.all
   end
 
-  def departure_airport
-    @departure_airport ||= Airport.find_by(id: params[:name])
+  def all_search_params_present_and_not_empty?
+    [:departure_airport_id, :arrival_airport_id, :num_tickets, "time(2i)", "time(3i)", "time(1i)"].all? do |key|
+      params.has_key?(key) && !params[key].empty?
+    end
   end
 
-  def arrival_airport
-    @arrival_airport ||= Airport.find_by(id: params[:arrival_airport_id])
+  def given_valid_date?
+    begin
+      Flight.array_to_date(params['time(2i)'], params['time(3i)'], params['time(1i)'])
+    rescue ArgumentError
+      return false
+    end
+  end
+
+  def given_upcoming_date?
+    Flight.array_to_date(params['time(2i)'], params['time(3i)'], params['time(1i)']) >= Date.today
+  end
+
+  def same_airports?
+    params[:departure_airport_id] == params[:arrival_airport_id]
+  end
+
+  def handle_search_error
+    flash.now.alert = "Error: "
+    flash.now.alert += "All fields in the search form are required. " unless all_search_params_present_and_not_empty?
+    flash.now.alert += "This date has already passed. " unless given_upcoming_date?
+    flash.now.alert += "Invalid date. " unless given_valid_date?
+    flash.now.alert += "The departure and arrival airports cannot be the same." if same_airports?
   end
 end
