fb43254dbce1e6f0cef7e6f4a3af7ad78fe272b0
Humberto <hdkaratechop@gmail.com>
2023-04-26 23:51:48 -0700

    add bootstrap

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,27 +1,24 @@
 class BookingsController < ApplicationController
-    def new
-      @booking = Booking.new
+  def new
+    @booking = Booking.new
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(allowed_post_params)
+    if @booking.save
+      redirect_to @booking, notice: 'Your flight was successfully booked.'
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  private 
   
-    def show
-      @booking = Booking.find(params[:id])
-    end
-  
-    def create
-      @booking = Booking.new(search_params)
-      if @booking.save
-        redirect_to @booking, notice: 'Your flight was successfully booked.'
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-  
-    private 
-    
-    def search_params
-      params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
-    end
+  def allowed_post_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+  end
 end
-
-
-  
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,24 +1,24 @@
 class FlightsController < ApplicationController
-  before_action :set_flights, only: [:index]
-
   def index
-    if params[:searching]
-      if all_search_params_present_and_not_empty? && given_valid_date? && given_upcoming_date? && !same_airports?
-        @flights = Flight.search(params)
-      else
-        handle_search_error
+    if params && all_search_params_present_and_not_empty? && given_valid_date? && given_upcoming_date? && !same_airports?
+      @flights = Flight.search(params)
+    elsif params[:searching]
+      if !all_search_params_present_and_not_empty?
+        flash.now.alert = "All fields in the search form are required."
+      elsif !given_upcoming_date?
+        flash.now.alert = "This date has already passed."
+      elsif !given_valid_date?
+        flash.now.alert = "Invalid date."
+      elsif same_airports?
+        flash.now.alert = "The departure and arrival airports cannot be the same."
       end
     end
   end
 
   private
 
-  def set_flights
-    @flights = Flight.all
-  end
-
   def all_search_params_present_and_not_empty?
-    [:departure_airport_id, :arrival_airport_id, :num_tickets, "time(2i)", "time(3i)", "time(1i)"].all? do |key|
+    [:departure_airport_id, :arrival_airport_id, :ticket_num, "time(2i)", "time(3i)", "time(1i)"].all? do |key|
       params.has_key?(key) && !params[key].empty?
     end
   end
@@ -37,13 +37,5 @@ class FlightsController < ApplicationController
 
   def same_airports?
     params[:departure_airport_id] == params[:arrival_airport_id]
-  end
-
-  def handle_search_error
-    flash.now.alert = "Error: "
-    flash.now.alert += "All fields in the search form are required. " unless all_search_params_present_and_not_empty?
-    flash.now.alert += "This date has already passed. " unless given_upcoming_date?
-    flash.now.alert += "Invalid date. " unless given_valid_date?
-    flash.now.alert += "The departure and arrival airports cannot be the same." if same_airports?
   end
 end
